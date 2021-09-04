0ee10c07ec937b5de3e6f4cd0a3b964ceed027fd
jhondeiberd <jhondeiberd@gmail.com>
2021-09-03 23:34:04 -0400

    Documents teacher

---
+++ Python_-_1.py
@@ -0,0 +1,71 @@
+# python string manipulations
+
+# print('hello world!')
+# print(1 + 2)
+# print(6 * 7)
+# print(type(1 + 2))
+# print('the end')
+# print("python's strings are easy to use")
+# # print('we can even include "quotes" in strings')
+# print('hello' + 'world')
+# greeting = 'hello'
+# name = 'dara'
+# print(greeting + name)
+
+# i have added a space in between the strings
+# print(greeting + ' ' + name)
+
+# this generate an error
+# greeting = 23.87
+# name='ali'
+# print(greeting + name)
+# print(str(greeting) + name)
+
+# greeting = 'hello'
+# name=input('please enter your name ')
+# print(greeting + ' ' + name)
+
+
+# splitString = "this string has been\nsplit over\nseveral\nlines"
+# print(splitString)
+#
+# tabbedString = "1\t2\t3\t4\t5\t"
+# print(tabbedString)
+
+# print('the guy said "no,no,\'e\'s uh,...he\'s huming"')
+# print("the guy said \"no,no,'e's uh,...he's huming\"")
+# print("""the guy said "no,no,'e's uh,...he's huming" """)
+# print('''the guy said "no,no,'e's uh,...he's huming" ''')
+
+# anotherSplitString = """this string has been
+# split over
+# sss
+# several lines"""
+# print(anotherSplitString)
+
+# _myName = 'dara'
+# 1myName = 'dara'  #this will generate an error
+# Dara42 = "Good"
+
+# Python is case sensitive
+# greeting = "greeting"
+# Greeting = "Greeting"
+# DAra42 = "hello"
+# print(DAra42 ,  greeting)
+# print(DAra42 , Greeting)
+
+# python Integers
+# a=12
+# b=3
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b) #return float by default
+# print(a//b) #return integer
+# print(a%b)
+#
+# #loop example
+# for i in range(1,4):
+#     print(i)
+
+# to add multiple lines comments: ctrl /
